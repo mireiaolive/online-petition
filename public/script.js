ca7340342fbb171f192bda8e165b330c95b02440
Mireia Olivé <mirisfu@gmail.com>
2021-07-16 21:21:24 +0200

    last changes

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
-//todataurl methode of canavas. when we have the url and we have to pass it wit val()
+//canvas.toDataURL(type, encoderOptions); methode of canavas. when we have the url and we have to pass it wit val()
+//https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
 //type hidden. At the petition page we have 3 inputs fields
 //when we do submit
-//we need a midelware we put the name atribute, look express notes. We have to rapp into a post
+//we need a middleware we put the name atribute, look express notes. We have to rapp into a post
--- server.js
+++ server.js
@@ -1,23 +1,55 @@
 const express = require("express");
 const app = express();
+const hb = require("express-handlebars");
 const db = require("./db");
+var cookieSession = require("cookie-session");
+
+app.engine("handlebars", hb());
+app.set("view engine", "handlebars");
 
 app.use(express.static("./public"));
 
-app.get("/cities", (req, res) => {
-    db.getCities()
+app.use(
+    cookieSession({
+        secret: `I'm always angry.`,
+        maxAge: 1000 * 60 * 60 * 24 * 14,
+        sameSite: true,
+    })
+);
+
+app.get("/petition", function (req, res) {
+    res.render("petition", {
+        layout: "main",
+    });
+});
+
+app.get("/petition", (req, res) => {
+    db.getData()
         .then(({ rows }) => {
             console.log("data from db: ", rows);
         })
-        .catch((err) => console.log("err in/cities: ", err));
+        .catch((err) => console.log("err in/data: ", err));
 });
 
-app.post("/add-city", (req, res) => {
+app.post("/petition", (req, res) => {
     //data that the user sent to us
-    db.addCity("Cuenca", "Ecuador")
+    db.addData("first", "last", "signature")
         .then(() => {
-            console.log("we added a city");
+            console.log("we added data");
         })
-        .catch((err) => console.log("err in post/add-city: ", err));
+        .catch((err) => console.log("err in post/add-data: ", err));
 });
+
+app.get("/thanks", function (req, res) {
+    res.render("thanks", {
+        layout: "main",
+    });
+});
+
+app.get("/signers", function (req, res) {
+    res.render("signers", {
+        layout: "main",
+    });
+});
+
 app.listen(8080, () => console.log("petition server is listening... "));
